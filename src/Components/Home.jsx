91229d891f6b22b55611e8b02d7958c0da2e3b80
Dinesh Reddy <ndkr2716@gmail.com>
2023-04-23 13:23:03 +0530

    Assignment Completed

---
+++ src/Components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <img src='https://webneel.com/wallpaper/sites/default/files/images/08-2018/3-nature-wallpaper-mountain.preview.jpg' alt='pic' width='100%' height='535px'/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <div className='nav'>
+      <div>
+        <h1>LOGO</h1>
+      </div>
+      <div className='links'>
+        <section>
+            <a href='#'>HOME</a>
+        </section>
+        <section>
+        <a href='#'>ABOUT</a>
+        </section>
+        <section>
+        <a href='#'>MENU</a>
+        </section>
+        <section>
+        <a href='#'>ORDER</a>
+        </section>
+        <section>
+        <a href='#'>CONTACT</a>
+        </section>
+      </div>
+    </div>
+  )
+}
+
+export default Nav
